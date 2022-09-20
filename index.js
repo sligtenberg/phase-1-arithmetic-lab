1741a26514054e3a2e133434b09012e3641a05d5
stephen ligtenberg <bountyhunter24@gmail.com>
2022-09-20 19:18:52 -0400

    commit tree 2b9cc205c42cdc88f491de11d195fb128dccaa87

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 6.2;
+const num2 = 10;
+const num3 = 44;
+const num4 = 10;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(10 ** 100 * Math.random());
+
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 20);
